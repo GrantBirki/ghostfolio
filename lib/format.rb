d238d9ffec6ffaf7c07d97ee3462aa7343a6cec4
GrantBirki <grantbirki@github.com>
2023-09-20 12:32:26 -0600

    fmt

---
+++ lib/format.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class Format
+  # soured from: https://github.com/ghostfolio/ghostfolio/blob/c511ec7e33123679490b36495d9ca7a09f04d329/apps/client/src/app/services/import-activities.service.ts#L16-L30
+  def initialize
+    @translations = {
+      date: {
+        inputs: ["Date"],
+        output: "date"
+      },
+      fee: {
+        inputs: ["Fee", "Fees & Comm"],
+        output: "fee"
+      },
+      quantity: {
+        inputs: ["Quantity"],
+        output: "quantity"
+      },
+      symbol: {
+        inputs: ["Symbol", "Ticker"],
+        output: "symbol"
+      },
+      type: {
+        inputs: ["Type"],
+        output: "type"
+      },
+      unit_price: {
+        inputs: ["Price"],
+        output: "price"
+      }
+    }
+  end
+end
